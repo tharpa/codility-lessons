408430af1400e2ed1a2ab2283902baafca932d49
rk <rafal.kowalski@mac.com>
2019-09-06 13:34:03 +0200

    Made it almost a one liner in Scala.
    Still, I have no idea how to do it purely functional.

--- src/main/scala/Codility02.scala
+++ src/main/scala/Codility02.scala
@@ -2,15 +2,9 @@ package codility02
 
 object Solution {
   def solution(a: Array[Int]): Int = {
-    val N = a.length
     val odd = scala.collection.mutable.Set[Int]()
 
-    var i = 0
-    while (i < N) {
-      if (odd contains a(i)) odd -= a(i) else odd += a(i)
-
-      i += 1
-    }
+    for (e <- a) if (odd contains e) odd -= e else odd += e
 
     // what to do with a case where the initial condition is violated?
     if (odd.nonEmpty) odd.head else -1
