6e76a294bf34c7b6e1e33bd7a7b8fa818dd9a4c2
rk <rafal.kowalski@mac.com>
2019-09-06 14:29:15 +0200

    Recursive call to shift by one.
    Using Scala collection operations... Maybe this is a bit cheaty?

--- src/main/scala/Codility03.scala
+++ src/main/scala/Codility03.scala
@@ -2,7 +2,10 @@ package codility03
 
 object Solution {
   def solution(a: Array[Int], k: Int): Array[Int] = {
+    def shiftRight(a: Array[Int]): Array[Int] = {
+       a.last +: a.dropRight(1)
+    }
 
-    Array()
+    if (k == 0) a else solution(shiftRight(a), k - 1)
   }
 }
